cea8d279d152c7ab45a45e51b8cb0858b4f9ae0a
Guzel <hamzina0092@gmail.com>
2020-05-19 23:33:53 +0300

    some changes for maven

---
+++ src/test/java/TestSuit.java
@@ -0,0 +1,14 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestSuit {
+
+    @Test
+    public void createTest(){
+        int a = 5;
+        int b = 5;
+
+        Assert.assertEquals(a,b);
+
+    }
+}
