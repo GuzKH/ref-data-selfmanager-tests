27a84dc0dadeccdffcebc8257a8a11d7f8161e05
aKupreychik <aKupreychik@alfabank.ru>
2020-05-29 22:38:12 +0300

    add Zoom notification 8

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -41,17 +41,16 @@ pipeline {
                 ])
             }
         }
-
-        post {
-            allways {
-                zoomSend([
-                        authToken : 'JuAlhTSVHx63U09d3K4dqvBnNF2faEArpXmk0SrCaba7PSTuAw8',
-                        webhookUrl: 'https://applications.zoom.us/addon/v2/jenkins/webhooks/yAdSATNRR8uBxH_EqSjQKw',
-                        message   : "FINISHED: Job '${env.JOB_NAME} #${env.BUILD_NUMBER}' (${env.BUILD_URL}) by " +
-                                "${currentBuild.getBuildCauses('hudson.model.Cause$UserIdCause').userName}." +
-                                "Status = ${currentBuild.result}, after ${currentBuild.duration}ms. Info = ${currentBuild.description}"
-                ])
-            }
+    }
+    post {
+        allways {
+            zoomSend([
+                    authToken : 'JuAlhTSVHx63U09d3K4dqvBnNF2faEArpXmk0SrCaba7PSTuAw8',
+                    webhookUrl: 'https://applications.zoom.us/addon/v2/jenkins/webhooks/yAdSATNRR8uBxH_EqSjQKw',
+                    message   : "FINISHED: Job '${env.JOB_NAME} #${env.BUILD_NUMBER}' (${env.BUILD_URL}) by " +
+                            "${currentBuild.getBuildCauses('hudson.model.Cause$UserIdCause').userName}." +
+                            "Status = ${currentBuild.result}, after ${currentBuild.duration}ms. Info = ${currentBuild.description}"
+            ])
         }
     }
 }
